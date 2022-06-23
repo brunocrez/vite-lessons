68afd4b2850868713a50209a0ce0edb6c78dfe87
Bruno Rezende <brunoc.rez@gmail.com>
2022-06-23 16:11:04 -0300

    feat: configuring layout and stylesheets

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import { gql, useQuery } from '@apollo/client'
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Lessons } from './pages/Lessons'
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY);  
-
   return (
-    <ul>
-    {data?.lessons.map(lesson => (
-      <li key={lesson.id}>{lesson.title}</li>
-    ))}
-    </ul>
+    <Lessons />
   )
 }
 
