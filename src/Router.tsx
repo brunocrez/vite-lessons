27496c77967044b943e247b41ef49edf3ea9f997
Bruno Rezende <brunoc.rez@gmail.com>
2022-06-24 15:31:16 -0300

    feat: subscribe page + creating new subscriber function + active lesson styles at sidebar

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
 import { Lessons } from './pages/Lessons';
+import { Subscribe } from './pages/Subscribe';
 
 export function Router() {
   return (
     <Routes>
-      <Route path='/' element={<h1>Home</h1>} />
+      <Route path='/' element={<Subscribe />} />
       <Route path='/lessons' element={<Lessons />} />
       <Route path='/lessons/lesson/:slug' element={<Lessons />} />
     </Routes>
